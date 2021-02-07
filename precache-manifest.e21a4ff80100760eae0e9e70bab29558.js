899a0ab9ead34456fcba8a406908a88f54b8579a
TouHouPolice <43275328+TouHouPolice@users.noreply.github.com>
2021-02-08 03:40:37 +0900

    Updates

---
+++ precache-manifest.e21a4ff80100760eae0e9e70bab29558.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2e3c4bb129880b0972fc16c9497852",
+    "url": "/cyberpunk-portfolio/index.html"
+  },
+  {
+    "revision": "f9fdae25375247ba8220",
+    "url": "/cyberpunk-portfolio/static/css/2.18ce6afa.chunk.css"
+  },
+  {
+    "revision": "311782923a0164c2c22c",
+    "url": "/cyberpunk-portfolio/static/css/main.97a60f0a.chunk.css"
+  },
+  {
+    "revision": "f9fdae25375247ba8220",
+    "url": "/cyberpunk-portfolio/static/js/2.ba49f754.chunk.js"
+  },
+  {
+    "revision": "1d37f81fad1ae1ec26479d98c67d3753",
+    "url": "/cyberpunk-portfolio/static/js/2.ba49f754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311782923a0164c2c22c",
+    "url": "/cyberpunk-portfolio/static/js/main.4411cdd4.chunk.js"
+  },
+  {
+    "revision": "492cf9f0f8c0ce870b3f",
+    "url": "/cyberpunk-portfolio/static/js/runtime-main.d48bda5e.js"
+  },
+  {
+    "revision": "24f66e8a95250a8d66c5088a5e117881",
+    "url": "/cyberpunk-portfolio/static/media/0.24f66e8a.JPG"
+  },
+  {
+    "revision": "6c057913488ba1198fcaee1db2b4f419",
+    "url": "/cyberpunk-portfolio/static/media/1.6c057913.JPG"
+  },
+  {
+    "revision": "4c48fce97b1edf64ba165935b154cb5d",
+    "url": "/cyberpunk-portfolio/static/media/10.4c48fce9.JPG"
+  },
+  {
+    "revision": "57aa9ecb4221ba10976fddb8696cd5f8",
+    "url": "/cyberpunk-portfolio/static/media/11.57aa9ecb.JPG"
+  },
+  {
+    "revision": "2434a5a7d8918cac3f53c431620f8a38",
+    "url": "/cyberpunk-portfolio/static/media/12.2434a5a7.JPG"
+  },
+  {
+    "revision": "1c991b53cd1de8776f863cf0bd59279d",
+    "url": "/cyberpunk-portfolio/static/media/13.1c991b53.JPG"
+  },
+  {
+    "revision": "25479db67da4682b99cb7230e2e7e5df",
+    "url": "/cyberpunk-portfolio/static/media/14.25479db6.JPG"
+  },
+  {
+    "revision": "c480e1bc58723aa3d815b93162303884",
+    "url": "/cyberpunk-portfolio/static/media/2.c480e1bc.JPG"
+  },
+  {
+    "revision": "1b8f40f9df8f56e3a263cdd360fd9227",
+    "url": "/cyberpunk-portfolio/static/media/3.1b8f40f9.JPG"
+  },
+  {
+    "revision": "dd7a9ec4b57be9c019e56acca9482701",
+    "url": "/cyberpunk-portfolio/static/media/4.dd7a9ec4.JPG"
+  },
+  {
+    "revision": "2d916b463ba9dbbf28523c074509c720",
+    "url": "/cyberpunk-portfolio/static/media/5.2d916b46.JPG"
+  },
+  {
+    "revision": "08eca96b9f9508367dacabc3a8893957",
+    "url": "/cyberpunk-portfolio/static/media/6.08eca96b.JPG"
+  },
+  {
+    "revision": "af1a11ca06142f5e36e3d263609db3df",
+    "url": "/cyberpunk-portfolio/static/media/7.af1a11ca.JPG"
+  },
+  {
+    "revision": "438dc484cfe2ce1cb2021da6fbce1be6",
+    "url": "/cyberpunk-portfolio/static/media/8.438dc484.JPG"
+  },
+  {
+    "revision": "de66d16b06d558c65de9248eb04cb06b",
+    "url": "/cyberpunk-portfolio/static/media/9.de66d16b.JPG"
+  },
+  {
+    "revision": "702217edd672b97abc1f6eae69c86dba",
+    "url": "/cyberpunk-portfolio/static/media/NIE_1.702217ed.JPG"
+  },
+  {
+    "revision": "d6b63980468d19b57f840c8af3d1d47f",
+    "url": "/cyberpunk-portfolio/static/media/NIE_2.d6b63980.JPG"
+  },
+  {
+    "revision": "a40ddcca35c69f832af4ea78f0614419",
+    "url": "/cyberpunk-portfolio/static/media/NIE_3.a40ddcca.JPG"
+  },
+  {
+    "revision": "2e5cc96979022d6d557e20c1a1e39b70",
+    "url": "/cyberpunk-portfolio/static/media/NN_1.2e5cc969.JPG"
+  },
+  {
+    "revision": "73fc0433120d4fd3ea8d70c67fa83c70",
+    "url": "/cyberpunk-portfolio/static/media/NN_2.73fc0433.JPG"
+  },
+  {
+    "revision": "f3b025e20ed99c83704da5636f727080",
+    "url": "/cyberpunk-portfolio/static/media/ask.f3b025e2.mp3"
+  },
+  {
+    "revision": "e5494c9b8c09cc1001fa9040b9bf902a",
+    "url": "/cyberpunk-portfolio/static/media/avatar.e5494c9b.jpg"
+  },
+  {
+    "revision": "40eadf869c4c36d6ddad316ebda6dec8",
+    "url": "/cyberpunk-portfolio/static/media/cherki_1.40eadf86.JPG"
+  },
+  {
+    "revision": "256628f5aa025b8bebafe3eab4bdf71d",
+    "url": "/cyberpunk-portfolio/static/media/cherki_2.256628f5.JPG"
+  },
+  {
+    "revision": "ef691e670f0f5b520eea28f6fec54166",
+    "url": "/cyberpunk-portfolio/static/media/click.ef691e67.mp3"
+  },
+  {
+    "revision": "dd97fbdc87af64457edbf7a393ccff70",
+    "url": "/cyberpunk-portfolio/static/media/deploy.dd97fbdc.mp3"
+  },
+  {
+    "revision": "20bac65c7597c39f34bd0f024983ba13",
+    "url": "/cyberpunk-portfolio/static/media/drone_1.20bac65c.png"
+  },
+  {
+    "revision": "608f8409b664194a3bc7d03e5d073a40",
+    "url": "/cyberpunk-portfolio/static/media/drone_2.608f8409.png"
+  },
+  {
+    "revision": "976d3dda543dab0bded239c0e1b2cd7d",
+    "url": "/cyberpunk-portfolio/static/media/drone_3.976d3dda.png"
+  },
+  {
+    "revision": "eaf4f1f6ae5778339d51f3dc836df103",
+    "url": "/cyberpunk-portfolio/static/media/dungeon_1.eaf4f1f6.png"
+  },
+  {
+    "revision": "e0018c7ca94bdd5b5b91336ad6c5836a",
+    "url": "/cyberpunk-portfolio/static/media/endless_1.e0018c7c.JPG"
+  },
+  {
+    "revision": "22879ceaf73040b79a3db3e53da258f9",
+    "url": "/cyberpunk-portfolio/static/media/endless_2.22879cea.JPG"
+  },
+  {
+    "revision": "e8be0673c04158fcac96e918cf85f7b7",
+    "url": "/cyberpunk-portfolio/static/media/error.e8be0673.mp3"
+  },
+  {
+    "revision": "58be846cb1331a8353d65b9a3c405966",
+    "url": "/cyberpunk-portfolio/static/media/fps_1.58be846c.JPG"
+  },
+  {
+    "revision": "36b3d3ac313a7ca99fd531dc80504f38",
+    "url": "/cyberpunk-portfolio/static/media/fps_2.36b3d3ac.JPG"
+  },
+  {
+    "revision": "70f3f977e0557f1c0a39637080e7baf0",
+    "url": "/cyberpunk-portfolio/static/media/gardens_1.70f3f977.JPG"
+  },
+  {
+    "revision": "ae1e19fe4baa0f4b50e42e2e320d5b0b",
+    "url": "/cyberpunk-portfolio/static/media/gardens_2.ae1e19fe.JPG"
+  },
+  {
+    "revision": "5048c14b55cedaa2adeca813677482a1",
+    "url": "/cyberpunk-portfolio/static/media/gardens_3.5048c14b.JPG"
+  },
+  {
+    "revision": "d57c8e65d2f251507b8f5e90a5254040",
+    "url": "/cyberpunk-portfolio/static/media/gardens_4.d57c8e65.JPG"
+  },
+  {
+    "revision": "701d08326b0f0e3e6ead186a7698600c",
+    "url": "/cyberpunk-portfolio/static/media/gardens_5.701d0832.JPG"
+  },
+  {
+    "revision": "eacd1e1c5ff11d44f3233e74422acaa8",
+    "url": "/cyberpunk-portfolio/static/media/gardens_6.eacd1e1c.JPG"
+  },
+  {
+    "revision": "ef5612c8070d2a4874ec551196fc1afc",
+    "url": "/cyberpunk-portfolio/static/media/glass_breaking.ef5612c8.ogg"
+  },
+  {
+    "revision": "d5788dba7648f582da8f796ecf949829",
+    "url": "/cyberpunk-portfolio/static/media/hack_1.d5788dba.JPG"
+  },
+  {
+    "revision": "97183591d836d313ab94ac2159a00142",
+    "url": "/cyberpunk-portfolio/static/media/hammer.97183591.png"
+  },
+  {
+    "revision": "46d0d67b72fe7e789ab571cb33f0d9ac",
+    "url": "/cyberpunk-portfolio/static/media/hit.46d0d67b.mp3"
+  },
+  {
+    "revision": "ed1b4d9a32dd97fc898fe3878461c368",
+    "url": "/cyberpunk-portfolio/static/media/hitachi.ed1b4d9a.jpg"
+  },
+  {
+    "revision": "8289359b44d8a109b24ad1ed00d61dcb",
+    "url": "/cyberpunk-portfolio/static/media/hol.8289359b.png"
+  },
+  {
+    "revision": "6d734cf2226fdbc89f570a5ad5e8cc51",
+    "url": "/cyberpunk-portfolio/static/media/hole.6d734cf2.png"
+  },
+  {
+    "revision": "55c3ea65e9cd269dd54a04bcf3bbd461",
+    "url": "/cyberpunk-portfolio/static/media/iive2d_1.55c3ea65.JPG"
+  },
+  {
+    "revision": "053817f276c1189cf9363fdb2ace41c7",
+    "url": "/cyberpunk-portfolio/static/media/iive2d_2.053817f2.JPG"
+  },
+  {
+    "revision": "947eb248ac15634e3c4388aa8eb0e94c",
+    "url": "/cyberpunk-portfolio/static/media/iive2d_3.947eb248.JPG"
+  },
+  {
+    "revision": "a81353df359dcf07bd65921d28e8cfe2",
+    "url": "/cyberpunk-portfolio/static/media/information.a81353df.mp3"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cyberpunk-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ab4a942d5c109d6357e55c2c1f3f57fb",
+    "url": "/cyberpunk-portfolio/static/media/machine_broken.ab4a942d.flac"
+  },
+  {
+    "revision": "9871288815089a36e9ab06f6bbfc8193",
+    "url": "/cyberpunk-portfolio/static/media/plat_1.98712888.JPG"
+  },
+  {
+    "revision": "7aafd15c4c561eedd792b370881397ce",
+    "url": "/cyberpunk-portfolio/static/media/plat_2.7aafd15c.JPG"
+  },
+  {
+    "revision": "95f07f44cb28f0c6eb8251989ed41fc2",
+    "url": "/cyberpunk-portfolio/static/media/screen.95f07f44.png"
+  },
+  {
+    "revision": "7ed8536eb782a253eb12284ce599cc89",
+    "url": "/cyberpunk-portfolio/static/media/typing.7ed8536e.mp3"
+  },
+  {
+    "revision": "a1ecd2b3225dcd214c30b7a5009b7f44",
+    "url": "/cyberpunk-portfolio/static/media/warning.a1ecd2b3.mp3"
+  },
+  {
+    "revision": "a1e28c8f2bb506049a926de3c137ad60",
+    "url": "/cyberpunk-portfolio/static/media/yolo_1.a1e28c8f.JPG"
+  },
+  {
+    "revision": "372d1456659c58f7325a1ca2e6445e72",
+    "url": "/cyberpunk-portfolio/static/media/yolo_2.372d1456.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.baaeed07b4303dce04a95349d17fc6e3.js"
+  "/cyberpunk-portfolio/precache-manifest.e21a4ff80100760eae0e9e70bab29558.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cyberpunk-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
