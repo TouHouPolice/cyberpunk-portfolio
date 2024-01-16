7f3d6e637d56a5b30e85da88530d9660983fd2b9
TouHouPolice <a237637249@hotmail.com>
2024-01-16 16:33:49 +0800

    remove node

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.scss";
 import {
   ThemeProvider,
